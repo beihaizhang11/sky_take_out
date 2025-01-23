b4cd01812e8ebfe03d1f2bf1ba34e9fa2479328e
zbh <bohan.zhang@ucdconnect.ie>
2025-01-23 17:52:38 +0800

    day2 封禁员工

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -34,4 +34,12 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 启用禁用员工账号
+     * @param employee
+     * @return
+     */
+    void update(Employee employee);
+
 }
